440da593f55a2464f1c34eb550a0ee8a98cdebb5
Lorenzo Masè <113705791+LorenzoMase@users.noreply.github.com>
2024-07-16 20:48:09 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,14 +1,13 @@
-
+import tensorflow as tf
 import pandas as pd
+from sklearn.model_selection import train_test_split
 dataset = pd.read_csv('dataset.csv')
 #dataset contains the whole csv file, pandas permits the read
 x = dataset.drop(columns=["label"])
 y = dataset["label"]
 #x contains all the feature's values, y contains all the real labels
-from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 #Splitting the Data into trainingSet e testingSet, with a 1/5 relation"""
-import tensorflow as tf
 model = tf.keras.models.Sequential()
 #Sequential model, optimized for 1 tensor input and 1 tensor output
 model.add(tf.keras.layers.Dense(256, input_shape=x_train.shape[1:], activation='sigmoid'))
